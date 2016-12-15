2c6d5ca8e4d18e521b4fc05dbb97852ab0dac4b5
48229 <48229@psdschools.org>
2016-12-15 11:52:28 -0700

    Added comments

--- src/test/Example.java
+++ src/test/Example.java
@@ -10,15 +10,14 @@ class Surface extends JPanel { //Text is drawn in Surface which inherits JPannel
 
     private void doDrawing(Graphics g) {//Void function takes in g from Old graphics class
 
-        Graphics2D g2d = (Graphics2D) g; //Extends the old Graphics class into the newer more advanced 
-        g2d.drawString("Java 2D", 50, 50);//Draws string on the pannel with the draw string method
+        Graphics2D g2d = (Graphics2D) g; //Extends the old Graphics class into the newer more advanced
+        g2d.drawString("Hello World", 50, 50);//Draws string on the panel with the draw string method
     }
 
-    @Override
-    public void paintComponent(Graphics g) {
-
-        super.paintComponent(g);
-        doDrawing(g);
+    @Override //paint component is overridden to allow super.paintCompnent to be called
+    public void paintComponent(Graphics g) { //Custom painting is performed inside the paintComponent() method
+    	super.paintComponent(g); //The super.paintComponent() method calls the method of the parent class, prepare a component for drawing
+        doDrawing(g); //The drawing is delegated inside the doDrawing() method
     }
 }
 
@@ -29,24 +28,24 @@ public class Example extends JFrame {
         initUI();
     }
 
-    private void initUI() {
+    private void initUI() { //initUI initiates the actual User Interface
 
-        add(new Surface());
+        add(new Surface());//This is where the surface is added JFrame container
 
-        setTitle("Simple Java 2D example");
-        setSize(300, 200);
+        setTitle("Java 2D");//Set title here
+        setSize(300, 200);//Sets size of window
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//The main method invokes the object with EventQueue, and runs the example object
 
-        EventQueue.invokeLater(new Runnable() {
+        EventQueue.invokeLater(new Runnable() { //Invokes as runnable that queues the events
 
             @Override
             public void run() {
-                Example ex = new Example();
-                ex.setVisible(true);
+                Example ex = new Example();//Creates a new object of this example type
+                ex.setVisible(true);//Sets the visibility of the created ex object to true
             }
         });
     }
